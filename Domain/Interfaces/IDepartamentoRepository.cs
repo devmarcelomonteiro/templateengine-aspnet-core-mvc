d296e6d7f5a9ddb1d73b9386139e902274ef3b87
Marcelo Monteiro <marcelomonteiro.dev@gmail.com>
2024-07-29 22:35:40 -0300

    Projeto criado

---
+++ Domain/Interfaces/IDepartamentoRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Domain.Entities;
+
+namespace Domain.Interface
+{
+    public interface IDepartamentoRepository
+    {
+        Task<IEnumerable<Departamento>> GetAllAsync();
+        Task<Departamento> GetByIdAsync(int id);
+        Task AddAsync(Departamento departamento);
+        Task UpdateAsync(Departamento departamento);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Infrastructure.Repositories/DepartamentoRepository.cs
@@ -0,0 +1,44 @@
+﻿
+using Domain.Entities;
+using Domain.Interface;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class DepartamentoRepository : IDepartamentoRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public DepartamentoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Departamento>> GetAllAsync()
+        {
+            return await _context.Departamentos.ToListAsync();
+        }
+
+        public Task AddAsync(Departamento departamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Departamento> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Departamento departamento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
